5a6f39fa242820ac2ab9c79f4812418991f77e69
Altynny <159096393+Altynny@users.noreply.github.com>
2025-03-04 18:13:49 +0800

    small fixes

--- tracky_tracky/setup.py
+++ tracky_tracky/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
    name='tracky_tracky',
-   version='1.1',
+   version='1.2',
    description='Provides a decorator for memory usage tracking. The part of FOSS course. Remastered by Ilya.',
    long_description_content_type='text/markdown',
    license='MIT',
-   author='Artem Vesnin',
-   author_email='artemvesnin@gmail.com',
-   url='https://github.com/standlab/mtracker',
+   author='Ilya Altynnikov',
+   author_email='altynnikovila@gmail.com',
+   url='https://github.com/Altynny/dev_culture/tree/main/tracky_tracky',
    packages=['mtracker'], 
    install_requires=[], # it is empty since we use standard python library
    extras_require={
