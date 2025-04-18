afed9c7c59500cf74959b51d934a7c2c51c14db4
Altynny <159096393+Altynny@users.noreply.github.com>
2025-04-18 15:19:59 +0800

    refactoring changes

--- refactoring/ants/route.py
+++ refactoring/ants/route.py
@@ -1,8 +1,17 @@
-class route:
-    def __init__(self, nodes: list, length: int, chance: float):
+class Route:
+    """
+    Represents a candidate path in the graph.
+    """
+
+    def __init__(self, nodes: list, length: float, probability: float):
+        """
+        :param nodes: Sequence of nodes in the route.
+        :param length: Total distance of the route.
+        :param probability: Product of branch probabilities.
+        """
         self.nodes = nodes
         self.length = length
-        self.chance = chance
+        self.probability = probability
 
     def __str__(self):
-        return f'length - {self.length}\n{self.nodes}'
+        return f"Route(length={self.length}, nodes={self.nodes})"
