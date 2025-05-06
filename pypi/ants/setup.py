c1ebbdd4c2dae7ad91374528bbdcd834ff5fcc90
Altynny <159096393+Altynny@users.noreply.github.com>
2025-05-06 12:48:22 +0800

    pypi_upload_changes
    - project prepared for upload
    - README and LICENSE files added
    - small fixes

--- pypi/ants/setup.py
+++ pypi/ants/setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
    name='ants',
    version='1.0',
    description='Finds the shortest route in graph via ant algorithm.',
+   long_description=long_description,
+   long_description_content_type='text/markdown',
    license='MIT',
    author='Ilya Altynnikov',
    author_email='altynnikovila@gmail.com',
@@ -13,7 +17,7 @@ setup(
    extras_require={
         'test': [
             'pytest',
-            # 'coverage',
+            'coverage',
         ],
    },
    python_requires='>=3',
